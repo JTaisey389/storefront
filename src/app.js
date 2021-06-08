e59df8db8f5666e12ad98faad334d15587a5b023
Jason Taisey <jdtaisey89@icloud.com>
2021-06-07 19:41:17 -0700

    inital functionality working

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from './components/header/header.js';
+import Footer from './components/footer/footer.js';
+import Products from './components/products/details.js';
+import './app.css';
+
+export default function App() {
+  return (
+    <>
+    <Header/>
+      <main>
+        <body>
+        <h1>Browse our Categories</h1>
+        <h3>Electronics | Food</h3>
+        <h1>ELECTRONICS</h1>
+        <p>Category Description Goes Here</p>
+        {/* <Products> */}
+        {/* </Products> */}
+        </body>
+      </main>
+    <Footer/>
+    </>
+  )
+};
